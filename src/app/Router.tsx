fad9deab3b455748d805951aa270ef66fdbb16bd
anonymousRecords <a.Records2022@gmail.com>
2026-01-29 23:46:08 +0900

    feat: 기본 라우팅 구조 및 AppRouter 구현

---
+++ src/app/Router.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router";
+import { Layout } from "../components/layout";
+import { DashboardPage } from "../pages/Dashboard/DashboardPage";
+import { ProblemDetailPage, ProblemsPage } from "../pages/Problems";
+import { SettingsPage } from "../pages/Settings";
+
+export function AppRouter() {
+	return (
+		<Layout>
+			<Routes>
+				<Route path="/" element={<DashboardPage />} />
+				<Route path="/problems" element={<ProblemsPage />} />
+				<Route path="/problems/new" element={<ProblemDetailPage />} />
+				<Route path="/problems/:id" element={<ProblemDetailPage />} />
+				<Route path="/settings" element={<SettingsPage />} />
+			</Routes>
+		</Layout>
+	);
+}
