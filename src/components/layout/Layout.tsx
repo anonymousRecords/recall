60110827fc874cbe2fdb6bc2db28faf288172660
anonymousRecords <a.Records2022@gmail.com>
2026-01-29 23:45:00 +0900

    feat: Layout 컴포넌트 구현

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,135 @@
+import type { ReactNode } from "react";
+import { NavLink } from "react-router";
+import { cn } from "../../lib/utils";
+
+interface LayoutProps {
+	children: ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+	return (
+		<div className="flex h-[600px] w-[400px] flex-col bg-white dark:bg-gray-950">
+			<main className="flex-1 overflow-auto">{children}</main>
+
+			<nav className="border-t h-[60px] border-gray-200 dark:border-gray-800 flex items-center justify-around">
+				<NavItem to="/" icon={<HomeIcon />} label="홈" />
+				<NavItem to="/problems" icon={<ListIcon />} label="문제" />
+				<NavItem to="/problems/new" icon={<PlusIcon />} label="추가" />
+				<NavItem to="/settings" icon={<SettingsIcon />} label="설정" />
+			</nav>
+		</div>
+	);
+}
+
+interface NavItemProps {
+	to: string;
+	icon: ReactNode;
+	label: string;
+}
+
+function NavItem({ to, icon }: NavItemProps) {
+	return (
+		<NavLink
+			to={to}
+			className={({ isActive }) =>
+				cn(
+					"flex flex-col items-center gap-0.5 px-4 py-8 transition-colors",
+					isActive
+						? "text-orange-400 dark:text-white"
+						: "text-gray-300 hover:text-orange-300 dark:text-gray-400 dark:hover:text-gray-200",
+				)
+			}
+		>
+			{icon}
+		</NavLink>
+	);
+}
+
+function HomeIcon() {
+	return (
+		<svg
+			role="img"
+			aria-label="Home"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			width={32}
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+			/>
+		</svg>
+	);
+}
+
+function ListIcon() {
+	return (
+		<svg
+			role="img"
+			aria-label="List"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			width={32}
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+			/>
+		</svg>
+	);
+}
+
+function PlusIcon() {
+	return (
+		<svg
+			role="img"
+			aria-label="Plus"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			width={32}
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+			/>
+		</svg>
+	);
+}
+
+function SettingsIcon() {
+	return (
+		<svg
+			role="img"
+			aria-label="Settings"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			width={32}
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+			/>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+			/>
+		</svg>
+	);
+}
