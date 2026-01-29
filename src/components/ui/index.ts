b9ff01710d81828fb8c2588e7ba7e954fe5c45f5
anonymousRecords <a.Records2022@gmail.com>
2026-01-29 23:41:58 +0900

    chore: ui 컴포넌트 일괄 export를 위한 index.ts 추가

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Badge } from "./Badge";
+export { Button } from "./Button";
+export { Card, CardContent, CardHeader, CardTitle } from "./Card";
+export { Input } from "./Input";
+export { Modal } from "./Modal";
+export { Select } from "./Select";
+export { Textarea } from "./Textarea";
