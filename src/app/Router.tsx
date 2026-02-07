ab714b6556f93ce1f81f70d081e5edc9e8d019f2
anonymousRecords <a.Records2022@gmail.com>
2026-02-07 19:35:52 +0900

    feat: LiveCodingPage 라우트 추가

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import { Layout } from "../components/layout";
 import { DashboardPage } from "../pages/Dashboard/DashboardPage";
+import { LiveCodingPage } from "../pages/LiveCoding";
 import { ProblemDetailPage, ProblemsPage } from "../pages/Problems";
 import { SettingsPage } from "../pages/Settings";
 
@@ -12,6 +13,7 @@ export function AppRouter() {
 				<Route path="/problems" element={<ProblemsPage />} />
 				<Route path="/problems/new" element={<ProblemDetailPage />} />
 				<Route path="/problems/:id" element={<ProblemDetailPage />} />
+				<Route path="/live" element={<LiveCodingPage />} />
 				<Route path="/settings" element={<SettingsPage />} />
 			</Routes>
 		</Layout>
