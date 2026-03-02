68ae790c04b116fae5be79914854a8a684c8bec7
anonymousRecords <a.Records2022@gmail.com>
2026-03-03 00:52:45 +0900

    fix: analytics에 queryClient 적용

--- entrypoints/analytics/main.tsx
+++ entrypoints/analytics/main.tsx
@@ -1,10 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AnalyticsPage } from "../../src/pages/Analytics/AnalyticsPage";
 import "../sidepanel/style.css";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 30,
+		},
+	},
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<AnalyticsPage />
+		<QueryClientProvider client={queryClient}>
+			<AnalyticsPage />
+		</QueryClientProvider>
 	</React.StrictMode>,
 );
