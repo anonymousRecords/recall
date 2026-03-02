f1c1715a6dde9f909a87e30219d2c4acde4e1d09
anonymousRecords <a.Records2022@gmail.com>
2026-03-02 15:05:28 +0900

    chore: tanstack query 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { MemoryRouter } from "react-router";
 import { AppRouter } from "./Router";
 
+const queryClient = new QueryClient();
+
 export function App() {
 	return (
-		<MemoryRouter initialEntries={["/"]}>
-			<AppRouter />
-		</MemoryRouter>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={true} />
+
+			<MemoryRouter initialEntries={["/"]}>
+				<AppRouter />
+			</MemoryRouter>
+		</QueryClientProvider>
 	);
 }
